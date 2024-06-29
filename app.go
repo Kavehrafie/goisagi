35f58391a5ca85e54a0eb91cebca4a490d028e35
kavehrafie <kavehrafie@gmail.com>
2024-06-29 16:50:00 -0500

    with local env check

--- app.go
+++ app.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/gofiber/fiber/v3/middleware/recover"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -15,13 +16,17 @@ func main() {
 
 	app.Use(logger.New())
 	app.Use(recover.New())
+
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString(os.Getenv("APP_NAME"))
 	})
 
-	// if err := godotenv.Load(".env"); err != nil {
-	// 	panic(err)
-	// }
+	// load .env file if not in production
+	if env := os.Getenv("RAILWAY_ENVIRONMENT_NAME"); env != "production" {
+		if err := godotenv.Load(".env"); err != nil {
+			panic(err)
+		}
+	}
 
 	port := os.Getenv("PORT")
 
